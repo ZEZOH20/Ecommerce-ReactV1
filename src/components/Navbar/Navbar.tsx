e61dff2896b8be25cb9281f306bcd426339bda07
mkhaled <mohammedkhalidmahmoud@gmail.com>
2025-03-26 02:08:50 +0200

    use generate-react-cli library

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './Navbar.module.css';
+
+function Navbar() {
+  return(
+    <>
+        <p>Hello from Navbar</p>
+    </>
+  )
+};
+
+export default Navbar;
