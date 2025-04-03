04f9688523748ec406397a62b3c935933cb5a626
zeyad <zeyad@>
2025-04-03 14:39:48 +0200

    addToCart - display ProductsCart

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import {userContext} from "../../Context/UserContext.tsx";
+import {useNavigate} from 'react-router-dom';
+import axios from "axios";
+import Card from "../Card/Card.tsx";
+import AlertBox from "../utilities/AlertBox/AlertBox.tsx";
+
 // import styles from './Cart.module.css';
 
 function Cart() {
+    const {token} = useContext(userContext);
+    const navigate = useNavigate();
+    const [productCart, setProductCart] = useState([]);
+    const [apiError, setApiError] = useState(null);
+    const getProductsCart = () => {
+        axios.get("https://ecommerce.routemisr.com/api/v1/cart", {
+            headers: {
+                "token": token
+            }
+        }).then((response) => {
+            const products = response.data.data.products;
+            console.log(products);
+            const simplifiedProducts = products.map((products) => ({
+                count: products.count,
+                price: products.price,
+                id: products["product"].id,
+                title: products["product"].title,
+                image: products["product"].imageCover,
+                rating: products["product"].ratingAverage,
+            }))
+            setProductCart(simplifiedProducts);
+        }).catch((e) => {
+            navigate('Page404')
+            setApiError(e.message)
+        });
+    }
+    useEffect(() => {
+        token ? getProductsCart() : navigate('/signin');
+    }, [])
+    return (
+        <>
+            <div className=" grid grid-cols-3 gap-2">
+                {productCart ? productCart.map((p) => (
 
-  useEffect(()=>{
-    axios.get()
-  },[])
-  return(
-    <>
-        <p>Hello from Cart</p>
-    </>
-  )
+                    <Card id={p.id} title={p.title}  price={p.price} img_src={p.image} rating={p.rating} />
+
+                    // </Card>
+                )) : <AlertBox> {apiError} </AlertBox>}
+            </div>
+        </>
+    )
 };
 
 export default Cart;
